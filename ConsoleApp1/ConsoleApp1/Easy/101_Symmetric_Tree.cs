db48a5b5cde3970c0966454eb831b848f185929b
Yerlan Karakulov <smartdevkz@mail.ru>
2020-12-09 13:15:58 +0300

    Runtime: 76 ms, faster than 100.00% of C# online submissions for Symmetric Tree. Memory Usage: 25.7 MB, less than 39.32% of C# online submissions for Symmetric Tree.

---
+++ ConsoleApp1/ConsoleApp1/Easy/101_Symmetric_Tree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy101
+{
+    class Solution
+    {
+        public bool IsSymmetric(TreeNode root)
+        {
+            if (root == null) return true;
+            return IsSymmetricPair(root.left, root.right);
+        }
+
+        public bool IsSymmetricPair(TreeNode p, TreeNode q)
+        {
+            if (p == null || q == null) return p == null && q == null;
+            return p.val == q.val && IsSymmetricPair(p.left, q.right) && IsSymmetricPair(p.right, q.left);
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
