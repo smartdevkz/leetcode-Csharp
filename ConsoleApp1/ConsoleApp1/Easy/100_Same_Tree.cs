b790f3bf8b64fde5be1bb342c44a34feba6a5d54
Yerlan Karakulov <smartdevkz@mail.ru>
2020-12-09 13:05:16 +0300

    Runtime: 92 ms, faster than 58.44% of C# online submissions for Same Tree. Memory Usage: 25 MB, less than 31.12% of C# online submissions for Same Tree.

---
+++ ConsoleApp1/ConsoleApp1/Easy/100_Same_Tree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy_100
+{
+    class Solution
+    {
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            return false;
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
