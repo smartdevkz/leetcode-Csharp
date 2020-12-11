ab813b9ceddc37654ba51889a55e0ff8ab4ef672
Yerlan Karakulov <smartdevkz@mail.ru>
2020-12-11 05:42:10 +0300

    168. Excel Sheet Column Title. time: 76ms-85.28%, memory: 23.2Mb-33.58%

---
+++ ConsoleApp1/ConsoleApp1/Easy/168_ExcelSheetColumn.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy168
+{
+    class Solution
+    {
+        public string ConvertToTitle(int n)
+        {
+            if (n <= 0) return "";
+            if (n <= 26) return ((char)(n + 64)).ToString();
+            var r = (n % 26);
+            var d = n / 26;
+            if (r == 0 && d > 0)
+            {
+                d--;
+                r = 26;
+            }
+            var rStr = ((char)(r + 64)).ToString();
+            if (d == 0) return rStr;
+            return (d > 26 ? ConvertToTitle(d) : ((char)(d + 64)).ToString()) + rStr;
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,11 +10,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var solution = new Easy19.Solution();
+            var solution = new Easy168.Solution();
 
-            var res = solution.RemoveNthFromEnd(solution.GetInitialData(), 1);
-            solution.DisplayListNode(res);
-            //Console.WriteLine(res);
+            var res = solution.ConvertToTitle(701);
+            //solution.DisplayListNode(res);
+            Console.WriteLine(res);
             //DisplayArray(res);
 
             Console.ReadKey();
