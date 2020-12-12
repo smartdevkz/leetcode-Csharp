4bec98fb285815c0de3b71f712dcc0a3f5a4560b
Yerlan Karakulov <smartdevkz@mail.ru>
2020-12-13 01:43:30 +0300

    412. FizzBuzz. time: 232ms=90.36%, memory: 33.9mb=85.89%

---
+++ ConsoleApp1/ConsoleApp1/Easy/412_FizzBuzz.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy412
+{
+    class Solution
+    {
+        public IList<string> FizzBuzz(int n)
+        {
+            var arr = new string[n]; 
+            for(int i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    arr[i - 1] = "FizzBuzz";
+                }
+                else if(i % 3 == 0)
+                {
+                    arr[i - 1] = "Fizz";
+                }
+                else if(i % 5 == 0)
+                {
+                    arr[i - 1] = "Buzz";
+                }
+                else
+                {
+                    arr[i - 1] = i.ToString();
+                }
+            }
+            return null;
+        }
+    }
+}
