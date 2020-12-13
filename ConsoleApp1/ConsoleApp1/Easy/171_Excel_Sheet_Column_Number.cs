81ae0890731cbd6bd7471b37b692e8ccf922579a
Yerlan Karakulov <smartdevkz@mail.ru>
2020-12-13 04:35:25 +0300

    171. Excel Sheet Column Number. rt:68ms=96.73%, m:25.5mb=33.06%

---
+++ ConsoleApp1/ConsoleApp1/Easy/171_Excel_Sheet_Column_Number.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy171
+{
+    class Solution
+    {
+        int titleToNumber(string s)
+        {
+            double res = 0;
+            for(int i = 0; i < s.Length; i++)
+            {
+                int n = (int)(s[s.Length - 1 - i]);
+                res += (Math.Pow(26, i) * n);
+            }
+            return (int)res;
+        }
+    }
+}
