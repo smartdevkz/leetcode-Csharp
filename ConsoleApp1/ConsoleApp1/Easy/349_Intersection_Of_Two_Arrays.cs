561309338c4cd5fa7e03906129794756443ebddd
Yerlan Karakulov <smartdevkz@mail.ru>
2020-12-13 03:00:15 +0300

    349. Intersection Of Two Arrays. rt:276ms=5.73%, m:33.2MB=5.10%

---
+++ ConsoleApp1/ConsoleApp1/Easy/349_Intersection_Of_Two_Arrays.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy349
+{
+    class Solution
+    {
+        public int[] Intersection(int[] nums1, int[] nums2)
+        {
+            if (nums1 == null || nums1.Length == 0 && nums2 != null) return nums2;
+            if (nums2 == null || nums2.Length == 0 && nums1 != null) return nums1;
+            return nums1.Length < nums2.Length ? GetIntersection(nums1, nums2) : GetIntersection(nums2, nums1);
+        }
+
+        public int[] GetIntersection(int[] a, int[] b)
+        {
+            var lst = new List<int>();
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (lst.Contains(a[i])) continue;
+                if (b.Contains(a[i])) lst.Add(a[i]);
+            }
+            return lst.ToArray();
+        }
+    }
+}
