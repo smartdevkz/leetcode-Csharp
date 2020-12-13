81736560da169c4c06889635b49ac00cff1d0809
Yerlan Karakulov <smartdevkz@mail.ru>
2020-12-13 20:28:03 +0300

    206. Reverse Linked List. rt:84m=95.92%, m=25.2MB=45.30%

---
+++ ConsoleApp1/ConsoleApp1/Easy/206_Reverse_Linked_List.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy206
+{
+    class Solution
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            if (head == null || head.next == null) return head;
+
+            ListNode node = null;
+            while (head != null)
+            {
+                node = new ListNode(head.val, node);
+                head = head.next;
+            }
+            return node;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
