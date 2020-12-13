29da5424430217f40e12fe43fa3d74622c66a033
Yerlan Karakulov <smartdevkz@mail.ru>
2020-12-13 04:13:59 +0300

    941. Valid Mountain Array. rt:124ms=98.39%, m:33.8mb=51.55%

---
+++ ConsoleApp1/ConsoleApp1/Easy/350_Intersection_Of_Two_Arrays_2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy350
+{
+    class Solution
+    {
+        public int[] Intersect(int[] nums1, int[] nums2)
+        {
+            return null;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Easy/941_Valid_Mountain_Array.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy941
+{
+    class Solution
+    {
+        public bool ValidMountainArray(int[] arr)
+        {
+            if (arr.Length < 2) return false;
+            if (arr[0] >= arr[1]) return false;
+            bool hasPeaked = false;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (hasPeaked)
+                {
+                    if (arr[i] >= arr[i - 1]) return false;
+                }
+                else
+                {
+                    if (arr[i] <= arr[i - 1])
+                    {
+                        if (arr[i] < arr[i - 1]) { hasPeaked = true; continue; }
+                        return false;
+                    }
+                }
+
+            }
+            return hasPeaked && true;
+        }
+    }
+}
