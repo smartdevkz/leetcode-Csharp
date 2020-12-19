18c855afe2cee6e6083a319aff0a10012b96e179
Yerlan Karakulov <smartdevkz@mail.ru>
2020-12-20 01:15:31 +0300

    557. Reverse Words in a string 3. rt:128ms=21.79%, m:35mb=43.59%

---
+++ ConsoleApp1/ConsoleApp1/Easy/557_ReverseWordsInAString3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy557
+{
+    class Solution
+    {
+        public string ReverseWords(string s)
+        {
+            var builder = new StringBuilder();
+            var wordBuilder = new StringBuilder();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != ' ') wordBuilder.Insert(0, s[i]);
+                else
+                {
+                    wordBuilder.Append(' ');
+                    builder.Append(wordBuilder.ToString());
+                    wordBuilder.Clear();
+
+                }
+            }
+            if (wordBuilder.Length > 0) builder.Append(wordBuilder.ToString());
+            return builder.ToString();
+        }
+    }
+}
