9e091a9eeafd31dc39519a0dd84781396655a672
Yerlan Karakulov <smartdevkz@mail.ru>
2020-12-19 21:53:49 +0300

    344. Reverse String. rt:408ms=8.80%, m:35.3mb=71.89%

---
+++ ConsoleApp1/ConsoleApp1/Easy/344_ReverseString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy344
+{
+    class Solution
+    {
+        public void ReverseString(char[] s)
+        {
+            int i = 0;
+            while (s.Length - 2 * i - 1 > 0)
+            {
+                var t = s[i];
+                s[i] = s[s.Length - 1 - i];
+                s[s.Length - 1 - i] = t;
+                i++;
+            }
+            return;
+        }
+    }
+}
