aa5cc11dfa408d2cf9ad056aedc9d1cc4f605267
Yerlan Karakulov <smartdevkz@mail.ru>
2020-12-19 16:35:26 +0300

    771. Jewels and Stones. rt:76ms=56.26%, m:23.8mb=81.85%

---
+++ ConsoleApp1/ConsoleApp1/Easy/771_JewelsAndStones.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy771
+{
+    class Solution
+    {
+        public int NumJewelsInStones(string J, string S)
+        {
+            if (J.Length == 0 || S.Length == 0) return 0;
+            int k = S.Length;
+            foreach (var c in J)
+            {
+                S = S.Replace(c.ToString(), "");
+            }
+            return k - S.Length;
+        }
+    }
+}
