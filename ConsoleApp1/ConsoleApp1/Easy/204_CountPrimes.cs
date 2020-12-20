653458d7b57517512199626a37ebcc5ff2b9cacc
Yerlan Karakulov <smartdevkz@mail.ru>
2020-12-20 14:11:28 +0300

    204. Count Primes. rt:408ms=23.51%, m:15.1mb=93.02%

---
+++ ConsoleApp1/ConsoleApp1/Easy/204_CountPrimes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy204
+{
+    class Solution
+    {
+        public int CountPrimes(int n)
+        {
+            if (n < 3) return 0;
+            if (n == 3) return 1;
+            int k = 1;
+            for (int i = 3; i < n; i += 2)
+            {
+                if (IsPrime(i)) k++;
+            }
+            return k;
+        }
+
+        private bool IsPrime(int n)
+        {
+            if (n < 4) return true;
+            if (n % 2 == 0) return false;
+            int i = 3;
+            while (i * i <= n)
+            {
+                if (n % i == 0) return false;
+                i += 2;
+            }
+            return true;
+        }
+    }
+}
