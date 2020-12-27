ebb1743cb6fa1c0887aae20f928e9b5bc0e565af
Yerlan Karakulov <smartdevkz@mail.ru>
2020-12-27 15:57:33 +0300

    237. Delete node in a Linked List. rt:92ms=77.58%, m:26.2mb=13%

---
+++ ConsoleApp1/ConsoleApp1/Easy/237_DeleteNodeInALinkedList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy237
+{
+    class Solution
+    {
+        public void DeleteNode(ListNode node)
+        {
+            if (node == null) return;
+            if (node.next != null)
+            {
+                node.val = node.next.val;
+                node.next = node.next.next;
+            }
+            else
+            {
+                node = null;
+            }
+        }
+    }
+
+    public class ListNode
+    {
+         public int val;
+         public ListNode next;
+         public ListNode(int x) { val = x; }
+     }
+}
