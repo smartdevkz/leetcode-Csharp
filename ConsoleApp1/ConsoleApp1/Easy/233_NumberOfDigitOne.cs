ada95070cd625fd84edc667c0f5dd92bae17e62d
Yerlan Karakulov <smartdevkz@mail.ru>
2020-12-27 11:50:05 +0300

    233: Number of digits. Could not solve.

---
+++ ConsoleApp1/ConsoleApp1/Easy/233_NumberOfDigitOne.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy233
+{
+    class Solution
+    {
+        public int CountDigitOne(int n)
+        {
+            int k = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                k += GetCount(i);
+            }
+            return k;
+        }
+
+        private int GetCount(int n)
+        {
+            int k = 0;
+            while (n > 0)
+            {
+                int r = n % 10;
+                if (r == 1) k++;
+                n = n / 10;
+            }
+
+            return k;
+        }
+    }
+}
