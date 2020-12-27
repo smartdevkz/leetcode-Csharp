840d4ce3829b5f89994a1c97dd02d48a1e30f5d1
Yerlan Karakulov <smartdevkz@mail.ru>
2020-12-27 17:16:47 +0300

    258. Add digits. rt:32ms=98.71%, m:15.2mb=93.55%

---
+++ ConsoleApp1/ConsoleApp1/Easy/258_AddDigits.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy258
+{
+    class Solution
+    {
+        public int AddDigits(int num)
+        {
+            int sum = 0;
+            while (num > 0)
+            {
+                var r = num % 10;
+                sum += r;
+                num = num / 10;
+                if (num == 0)
+                {
+                    num = sum;
+                    if (num / 10 == 0) break;
+                    sum = 0;
+                }
+            }
+            return num;
+        }
+
+    }
+}
