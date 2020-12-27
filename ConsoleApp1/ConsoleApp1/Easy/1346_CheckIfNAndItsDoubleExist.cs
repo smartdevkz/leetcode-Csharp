d9353bf74a54422ad3f8cf862f989f3a2653f04d
Yerlan Karakulov <smartdevkz@mail.ru>
2020-12-27 03:58:47 +0300

    1346. Check if N and Its double exist. rt:92ms=81.01%, m:25.6mb=78.85%

---
+++ ConsoleApp1/ConsoleApp1/Easy/1346_CheckIfNAndItsDoubleExist.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy1346
+{
+    class Solution
+    {
+        public bool CheckIfExist(int[] arr)
+        {
+            if (arr.Length < 2) return false;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (IsExist(arr[i], arr[j])) return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool IsExist(int a, int b)
+        {
+            if (a < 0 && b > 0) return false;
+            if (b < 0 && a > 0) return false;
+            if (a < 0) return IsExist((-1) * b, (-1) * a);
+            if (a >= 0 && a < b) return IsExist(b, a);
+            return b == (a - b);
+        }
+    }
+}
