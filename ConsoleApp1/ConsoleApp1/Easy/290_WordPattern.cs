56d8ea610bd8bb7fff10256c15ad62c9e367d5aa
Yerlan Karakulov <smartdevkz@mail.ru>
2020-12-28 17:48:51 +0600

    290. Word Pattern. rt:72ms=97.40%, m:22.6mb=66.15%

---
+++ ConsoleApp1/ConsoleApp1/Easy/290_WordPattern.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy290
+{
+    class Solution
+    {
+        public bool WordPattern(string pattern, string s)
+        {
+            var arr = s.Split(' ');
+            if (pattern.Length != arr.Length) return false;
+            var dict = new Dictionary<char, string>();
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                if (dict.ContainsKey(pattern[i]))
+                {
+                    if (dict[pattern[i]] != arr[i]) return false;
+                }
+                else
+                {
+                    if (dict.ContainsValue(arr[i])) return false;
+                    dict.Add(pattern[i], arr[i]);
+                }
+            }
+            return true;
+        }
+    }
+}
