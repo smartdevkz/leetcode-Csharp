a4a077ce1821ce2d9e7d2eb724f0c47c8e376e73
Yerlan Karakulov <smartdevkz@mail.ru>
2020-12-29 17:59:21 +0600

    20. Valid Parentheses. rt:112ms=6.04%, m:42mb=5.15%

---
+++ ConsoleApp1/ConsoleApp1/Easy/20_ValidParentheses.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy
+{
+    public class Solution
+    {
+        public bool IsValid(string s)
+        {
+            int prevLen = 0;
+            do
+            {
+                prevLen = s.Length;
+                s = s.Replace("()", "").Replace("[]", "").Replace("{}", "");                
+            } while (prevLen != s.Length);
+            return s.Length == 0;
+        }
+    }
+}
