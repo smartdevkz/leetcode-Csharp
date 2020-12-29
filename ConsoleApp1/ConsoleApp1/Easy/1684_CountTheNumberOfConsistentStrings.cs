dfeaaf74d8b60db8e1dec0fc51900f84ba00d5ee
smartdevkz <smartdevkz@mail.ru>
2020-12-29 18:02:34 +0600

    1684. Count the Number of Consistent Strings. rt:140ms=61.72%, m:37.4mb=83.45%

---
+++ ConsoleApp1/ConsoleApp1/Easy/1684_CountTheNumberOfConsistentStrings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy1684
+{
+    public class Solution
+    {
+        public int CountConsistentStrings(string allowed, string[] words)
+        {
+            int k = words.Length;
+            foreach (var w in words)
+            {
+                foreach (var c in w)
+                {
+                    if (!allowed.Contains(c))
+                    {
+                        k--;
+                        break;
+                    }
+                }
+            }
+            return k;
+        }
+    }
+}
