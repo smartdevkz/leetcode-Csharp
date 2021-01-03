fdbbbd16b7d460a48ae3df81b48fa12e67dfc63e
smartdevkz <ereke_enu@mail.ru>
2021-01-03 15:25:41 +0600

    2. Add two numbers. rt:108ms=57.61%, m:28.6mb=9.96%

---
+++ ConsoleApp1/ConsoleApp1/Easy/2_AddTwoNumbers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy2
+{
+    public class Solution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            List<int> lst = new List<int>();
+            int r = 0;
+            while (l1 != null || l2 != null)
+            {
+                var a = l1 != null ? l1.val : 0;
+                var b = l2 != null ? l2.val : 0;
+                var sum = a + b + r;
+                lst.Add(sum % 10);
+                r = sum / 10;
+                if (l1 != null) l1 = l1.next;
+                if (l2 != null) l2 = l2.next;
+            }
+            if (r > 0) lst.Add(r);
+            return Init(lst.ToArray());
+        }
+
+        public ListNode Init(int[] arr)
+        {
+            ListNode node = null;
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                node = new ListNode(arr[i], node);
+            }
+            return node;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ConsoleApp1.Easy206;
+using ConsoleApp1.Easy2;
 
 namespace ConsoleApp1
 {
@@ -12,17 +12,23 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var solution = new Easy43.Solution();
+            //test();
+            run();
+            Console.ReadKey();
+        }
 
-            var res = solution.Multiply("123","456");
+        static void run()
+        {
+            var solution = new Easy2.Solution();
+
+            var res = solution.AddTwoNumbers(solution.Init(new int[] { 9, 9, 9, 9, 9, 9, 9 }), solution.Init(new int[] { 9, 9, 9, 9 }));
             //DisplayArray(a);
 
             //var res = solution.CommonChars(new[] { "bella", "label", "roller" });
             //solution.DisplayListNode(res);
-            Console.WriteLine(res);
+            //Console.WriteLine(res);
             //DisplayArray(res);
-            //DisplayLinkedList(res);
-            Console.ReadKey();
+            DisplayLinkedList(res);
         }
 
         static void DisplayArray(IList<string> arr)
@@ -41,6 +47,21 @@ namespace ConsoleApp1
                 head = head.next;
                 if (head != null) Console.Write("->");
             }
+        }
+
+        static void test()
+        {
+            var res = getSum(35, 118);
+        }
+
+        static int getSum(int a, int b)
+        {
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+            Console.WriteLine(Convert.ToString(a, 2));
+            Console.WriteLine(Convert.ToString(b, 2));
+            Console.WriteLine();
+            return b == 0 ? a : getSum(a ^ b, (a & b) << 1); //be careful about the terminating condition;
         }
     }
 }
