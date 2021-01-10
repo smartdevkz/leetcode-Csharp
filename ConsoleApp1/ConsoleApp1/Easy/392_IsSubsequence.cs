9770f2827fb2aad13cd9267e878190286f6d344d
smartdevkz <ereke_enu@mail.ru>
2021-01-10 14:17:56 +0600

    392. Is subsequence. rt:144ms=5.28%, m:22.6mb=32.04%

---
+++ ConsoleApp1/ConsoleApp1/Easy/392_IsSubsequence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy392
+{
+    public class Solution
+    {
+        public bool IsSubsequence(string s, string t)
+        {
+            foreach (var c in s)
+            {
+                if (string.IsNullOrEmpty(t)) return false;
+                var i = t.IndexOf(c);
+                if (i == -1) return false;
+                i++;
+                t = i < t.Length ? t.Substring(i) : null;
+            }
+            return true;
+        }
+    }
+}
