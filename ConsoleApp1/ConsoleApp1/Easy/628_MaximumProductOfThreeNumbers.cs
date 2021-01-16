51ddf8771222473949e4b9561e88f0054dcaca8b
smartdevkz <ereke_enu@mail.ru>
2021-01-17 00:43:12 +0600

    628. Maximum product of three number

---
+++ ConsoleApp1/ConsoleApp1/Easy/628_MaximumProductOfThreeNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy628
+{
+    public class Solution
+    {
+        public int MaximumProduct(int[] nums)
+        {
+            Array.Sort(nums);
+            int max= nums[nums.Length - 1] * nums[nums.Length - 2] * nums[nums.Length - 3];
+            if (nums.Length > 2 && nums[0] < 0 && nums[1] < 0)
+            {
+                int max2 = nums[nums.Length - 1] * nums[0] * nums[1];
+                if (max2 > max) return max2;
+            }
+            return max;
+        }
+    }
+}
