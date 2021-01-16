3311e10297ee70c4239dc6aa8624a909b5efc8fe
smartdevkz <ereke_enu@mail.ru>
2021-01-17 01:41:10 +0600

    645. Set Mismatch. rt:284ms=43.82%, m:38.3=53.93%

---
+++ ConsoleApp1/ConsoleApp1/Easy/645_SetMisMatch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy645
+{
+    public class Solution
+    {
+        public int[] FindErrorNums(int[] nums)
+        {
+            Array.Sort(nums);
+            int needed = -1;
+            if (nums[nums.Length - 1] < nums.Length) needed = nums.Length;
+            if (nums[0] != 1) needed = 1;
+            int repeated = -1;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                int d = nums[i] - nums[i - 1];
+                if (needed == -1 && d > 1)
+                {
+                    needed = nums[i]-1;
+                }
+                if (repeated == -1 && d == 0)
+                {
+                    repeated = nums[i];
+                }
+                if (needed >= 0 && repeated >= 0) break;
+            }
+            return new int[] { repeated, needed };
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -19,10 +19,11 @@ namespace ConsoleApp1
 
         static void run()
         {
-            var solution = new Easy492.Solution();
+            var solution = new Easy645.Solution();
 
             //var res = solution.HammingWeight(43261596);
-            var res = solution.ConstructRectangle(37);
+            var res = solution.FindErrorNums(new int[] { 37, 62, 43, 27, 12, 66, 36, 18, 39, 54, 61, 65, 47, 32, 23, 2, 46, 8, 4, 24, 29, 38, 63, 39, 25, 11, 45, 28, 44, 52, 15, 30, 21, 7, 57, 49, 1, 59, 58, 14, 9, 40, 3, 42, 56, 31, 20, 41, 22, 50, 13, 33, 6, 10, 16, 64, 53, 51, 19, 17, 48, 26, 34, 60, 35, 5
+});
             //DisplayArray(a);
 
             //var res = solution.CommonChars(new[] { "bella", "label", "roller" });
