7f8ac1e88d3bf09ad2c19ec03f4cfc8da9bd41a0
smartdevkz <ereke_enu@mail.ru>
2021-01-16 22:04:48 +0600

    441. Arranging coins. rt:72ms=10.57%, m:15.3mb=43.09%

---
+++ ConsoleApp1/ConsoleApp1/Easy/441_ArrangingCoins.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy441
+{
+    public class Solution
+    {
+        public int ArrangeCoins(int n)
+        {
+            int k = 0;
+            while (n >= 0)
+            {
+                k++;
+                n -= k;
+            }
+            return k - 1;
+        }
+    }
+}
