0faecafb73cb1870641f52b0408d9773f56af2b7
smartdevkz <ereke_enu@mail.ru>
2021-01-16 23:02:41 +0600

    507. Perfect number. rt:36ms=91.95%, m:15.1mb=66.67%

---
+++ ConsoleApp1/ConsoleApp1/Easy/507_PerfectNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy507
+{
+    public class Solution
+    {
+        public bool CheckPerfectNumber(int num)
+        {
+            if (num == 1) return false;
+            int r = (int)Math.Sqrt(num);
+            int sum = 1;
+            for(int a = 2; a <= r; a++)
+            {
+                if (num % a == 0)
+                {
+                    sum += a;
+                    var b = num / a;
+                    if (a != b) sum += b;
+                }
+            }
+            return sum == num;
+        }
+    }
+}
