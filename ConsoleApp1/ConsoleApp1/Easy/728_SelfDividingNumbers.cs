763ca5b516f37d185443678b2cc3f0261e7bdee7
smartdevkz <ereke_enu@mail.ru>
2021-01-17 13:03:50 +0600

    728. Self dividing numbers. rt:196ms=96.30%, m:26.3mb=54.07%

---
+++ ConsoleApp1/ConsoleApp1/Easy/728_SelfDividingNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy728
+{
+    public class Solution
+    {
+        public IList<int> SelfDividingNumbers(int left, int right)
+        {
+            List<int> lst = new List<int>();
+            for(int i = left; i <= right; i++)
+            {
+                if (IsSelfDividing(i)) lst.Add(i);
+            }
+            return lst;
+        }
+
+        private bool IsSelfDividing(int n)
+        {
+            if (n / 10 == 0) return true;
+            var origin = n;
+            while (n > 0)
+            {
+                int d = n % 10;
+                if (d == 0 || origin % d != 0) return false;
+                n = n / 10;
+            }
+            return true;
+        }
+    }
+}
