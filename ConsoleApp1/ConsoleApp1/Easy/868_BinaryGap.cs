bcf3b7cbb3e6a23148613e029b00718a56113b38
smartdevkz <ereke_enu@mail.ru>
2021-01-17 15:29:51 +0600

    868. Binary Gap. rt:36ms=96.88%, mu:15.5mb=6.25%

---
+++ ConsoleApp1/ConsoleApp1/Easy/868_BinaryGap.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy868
+{
+    public class Solution
+    {
+        public int BinaryGap(int n)
+        {
+            var str = Convert.ToString(n, 2);
+            int max = 0;
+            bool hasStarted = false;
+            int d = 0;
+            foreach(var c in str)
+            {
+                if (c == '1')
+                {
+                    if (hasStarted)
+                    {
+                        d++;
+                        if (d > max) max = d;
+                        d = 0;
+                    }
+                    else
+                    {
+                        hasStarted = true;
+                    }
+                }
+                else
+                {
+                    if (hasStarted)
+                    {
+                        d++;
+                    }
+                }
+            }
+            return max;
+        }
+    }
+}
