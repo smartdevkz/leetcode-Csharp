b4de5228a14a00cba84f7c28b3ef96e7c3c39ca7
smartdevkz <ereke_enu@mail.ru>
2021-01-17 21:01:31 +0600

    1037. Valid boomerang. rt:88ms=87.50%, mu:25.3mb=15.63%

---
+++ ConsoleApp1/ConsoleApp1/Easy/1037_ValidBoomerang.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy1037
+{
+    public class Solution
+    {
+        public bool IsBoomerang(int[][] points)
+        {
+            var x1 = points[0][0];
+            var y1 = points[0][1];
+
+            var x2 = points[1][0];
+            var y2 = points[1][1];
+
+            var x3 = points[2][0];
+            var y3 = points[2][1];
+
+            return (y2 - y3) * (x1 - x3) - (y1 - y3) * (x2 - x3) == 0;
+        }
+    }
+}
