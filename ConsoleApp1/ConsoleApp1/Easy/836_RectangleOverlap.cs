b83d5eb7854274caab85fd667e45884a7bd57deb
smartdevkz <ereke_enu@mail.ru>
2021-01-17 15:05:20 +0600

    836. Rectangle Overlap. rt:92ms=42.86%,m:24.8mb=29.67%

---
+++ ConsoleApp1/ConsoleApp1/Easy/836_RectangleOverlap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy836
+{
+    public class Solution
+    {
+        public bool IsRectangleOverlap(int[] rec1, int[] rec2)
+        {
+            //x:0,2
+            //y:1,3
+            return !((rec1[0] >= rec2[0] && rec1[0] >= rec2[2])
+                || (rec1[2] <= rec2[0] && rec1[2] <= rec2[2])
+                || (rec1[1] >= rec2[1] && rec1[1] >= rec2[3])
+                || (rec1[3] <= rec2[1] && rec1[3] <= rec2[3]))
+                && IsRectangle(rec1) && IsRectangle(rec2);
+        }
+
+        private bool IsRectangle(int[] rec)
+        {
+            return rec[0] != rec[2] && rec[1] != rec[3];
+        }
+    }
+}
