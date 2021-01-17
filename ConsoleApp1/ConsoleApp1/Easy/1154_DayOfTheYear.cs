fb08c5b9df8d90eb7f9e65bd5a92d2df3bbe8877
smartdevkz <ereke_enu@mail.ru>
2021-01-17 21:09:53 +0600

    1154. Day of the year. rt:128ms=8.00%, mu:22.7mb=100.00%

---
+++ ConsoleApp1/ConsoleApp1/Easy/1154_DayOfTheYear.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy1154
+{
+    public class Solution
+    {
+        public int DayOfYear(string date)
+        {
+            var arr = date.Split('-');
+            int year = Convert.ToInt32(arr[0]);
+            int month = Convert.ToInt32(arr[1]);
+            int day = Convert.ToInt32(arr[2]);
+            for (int i = 1; i < month; i++)
+            {
+                day += DateTime.DaysInMonth(year, i);
+            }
+            return day;
+        }
+    }
+}
