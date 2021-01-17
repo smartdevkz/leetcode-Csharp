690f40331900cce48166cd29be13cb112b9a48b3
smartdevkz <ereke_enu@mail.ru>
2021-01-17 13:16:24 +0600

    1009. Complement of base 10 integer. rt:40ms=56.52%, 15.2mb=78.26%

---
+++ ConsoleApp1/ConsoleApp1/Easy/1009_ComplementOfBase10Integer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy1009
+{
+    public class Solution
+    {
+        public int BitwiseComplement(int N)
+        {
+            if (N == 0) return 0;
+            var str = Convert.ToString(N, 2);
+            var builder = new StringBuilder();
+            foreach (var c in str)
+            {
+                builder.Append(c == '0' ? '1' : '0');
+            }
+            return Convert.ToInt32(builder.ToString(), 2);
+        }
+    }
+}
