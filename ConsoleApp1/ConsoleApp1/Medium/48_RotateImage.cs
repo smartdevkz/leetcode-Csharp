c2e9e21e1325dda7e32545d75b279cb064f0af33
smartdevkz <ereke_enu@mail.ru>
2021-01-27 11:49:16 +0600

    48. Rotate Image. rt:420ms=5.31%, mu:31mb=68.53%

---
+++ ConsoleApp1/ConsoleApp1/Medium/48_RotateImage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Medium48
+{
+    public class Solution
+    {
+        public void Rotate(int[][] matrix)
+        {
+            int n = matrix.Length;
+            var origin = InitArr(n, matrix);
+            for (int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    matrix[j][n - 1 - i] = origin[i][j];
+                }
+            }
+        }
+
+        public int[][] InitArr(int n, int[][] matrix)
+        {
+            var arr = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = new int[n];
+            }
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    arr[i][j] = matrix[i][j];
+                }
+            }
+            return arr;
+        }
+    }
+}
