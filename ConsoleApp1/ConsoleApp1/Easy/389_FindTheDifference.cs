bafa6968b4aef5c82d6f1e2897f48c15c62fb224
Yerlan Karakulov <smartdevkz@mail.ru>
2021-01-31 18:48:14 +0300

    389. Find the difference. rt:96ms=36.11%, mu:25.4mb=62.22%

---
+++ ConsoleApp1/ConsoleApp1/Easy/389_FindTheDifference.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy389
+{
+    public class Solution
+    {
+        public char FindTheDifference(string s, string t)
+        {
+            var dict = new Dictionary<char, int>();
+            foreach(var c in s)
+            {
+                if (dict.ContainsKey(c))
+                {
+                    dict[c]++;
+                }
+                else
+                {
+                    dict.Add(c, 1);
+                }
+            }
+
+            foreach(var c in t)
+            {
+                if (dict.ContainsKey(c))
+                {
+                    if (dict[c] == 0) return c;
+                    dict[c]--;
+                }
+                else
+                {
+                    return c;
+                }
+            }
+            return ' ';
+        }
+    }
+}
