baf82fac192352e6ee93f47e6355e751b557ca06
Yerlan Karakulov <smartdevkz@mail.ru>
2021-02-02 23:28:29 +0300

    217. Contains Duplicate. rt:140ms=26.59%, mu:31.8mb=65.26%

---
+++ ConsoleApp1/ConsoleApp1/Easy/217_ContainsDuplicate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy217
+{
+    public class Solution
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            var hs = new HashSet<int>();
+            foreach(var item in nums)
+            {
+                if (hs.Contains(item))
+                {
+                    return true;
+                }
+                else
+                {
+                    hs.Add(item);
+                }
+            }
+            return false;
+        }
+    }
+}
