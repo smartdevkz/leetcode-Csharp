b713a41c495d038269646e3a37a6efbe980b8c56
smartdevkz <ereke_enu@mail.ru>
2021-02-03 13:42:30 +0600

    509. Fibonacci Number. rt:40ms=83.91%, mu:15.2mb=67.26%

---
+++ ConsoleApp1/ConsoleApp1/Easy/509_FibonacciNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy509
+{
+    public class Solution
+    {
+        public int Fib(int n)
+        {
+            if (n <= 1) return n;
+            int a = 0, b = 1;
+            for(int i = 2; i <= n; i++)
+            {
+                int c = b;
+                b = a + b;
+                a = c;
+            }
+            return b;
+        }
+
+        /*
+         * 52ms, 15.4mb
+         Solution 1
+
+        public int Fib(int n)
+        {
+            if (n <= 1) return n;
+            return Fib(n-1)+Fib(n-2);
+        }
+         */
+    }
+}
