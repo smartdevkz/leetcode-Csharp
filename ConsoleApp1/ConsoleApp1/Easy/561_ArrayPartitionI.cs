6a7058c3cb537c533e07be608cd00b86b8a52c9a
smartdevkz <ereke_enu@mail.ru>
2021-02-04 16:51:45 +0600

    561. Array partition I. rt:148ms=98.14%, 35.9mb=25.47%

---
+++ ConsoleApp1/ConsoleApp1/Easy/561_ArrayPartitionI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy561
+{
+    public class Solution
+    {
+        public int ArrayPairSum(int[] nums)
+        {
+            Array.Sort(nums);
+            int min = 0;
+            for(int i = 0; i < nums.Length; i += 2)
+            {
+                min += nums[i];
+            }
+            return min;
+        }
+    }
+}
