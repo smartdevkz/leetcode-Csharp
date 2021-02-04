5d6afc7eb4498ddac35ab9e37d759523e2bded1b
smartdevkz <ereke_enu@mail.ru>
2021-02-04 15:34:03 +0600

    1137. N-th Tribonacci Number. rt:64ms=12.35%, mu:15.2mb=30.25%

---
+++ ConsoleApp1/ConsoleApp1/Easy/1137_Nth_Tribonacci_Number.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Easy1137
+{
+    public class Solution
+    {
+        public int Tribonacci(int n)
+        {
+            int a = 0, b = 1, c = 1;
+            for(int i = 3; i <= n; i++)
+            {
+                int newA = b;
+                int newB = c;
+                c = a + b + c;
+                a = newA;
+                b = newB;
+            }
+            return c;
+        }
+    }
+}
