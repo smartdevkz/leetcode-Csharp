a7e10849c6695f4fd47f749c6dd92e2f4c85346e
Yerlan Karakulov <smartdevkz@mail.ru>
2021-03-02 11:57:12 +0300

    455. Assign cookies. rt: 128ms=84.17%, mu:30.7mb=85.00%

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ConsoleApp1.Easy2;
 
 namespace ConsoleApp1
 {
@@ -18,9 +17,9 @@ namespace ConsoleApp1
 
         static void run()
         {
-            var solution = new Easy1046.Solution();
+            var solution = new ConsoleApp1.Easy455.Solution();
 
-            var res = solution.LastStoneWeight(new int[] { 4,7,4,5,10,2,2,3 });
+            var res = solution.FindContentChildren(new int[]{1,2,3}, new int[]{4});
 
             Console.WriteLine(res);
 
@@ -41,34 +40,5 @@ namespace ConsoleApp1
             }
         }
 
-        static void DisplayLinkedList(ListNode head)
-        {
-            while (head != null)
-            {
-                Console.Write(head.val);
-                head = head.next;
-                if (head != null) Console.Write("->");
-            }
-        }
-
-        static void test()
-        {
-            int a = 5;
-            Console.WriteLine(Convert.ToString(a, 2));
-            //Console.WriteLine(Convert.ToString(a & 4, 2));
-            Console.WriteLine(Convert.ToString(a << 1, 2));
-            Console.WriteLine(Convert.ToString(a >> 1, 2));
-            //var res = getSum(35, 118);
-        }
-
-        static int getSum(int a, int b)
-        {
-            //Console.WriteLine(a);
-            //Console.WriteLine(b);
-            Console.WriteLine(Convert.ToString(a, 2));
-            Console.WriteLine(Convert.ToString(b, 2));
-            Console.WriteLine();
-            return b == 0 ? a : getSum(a ^ b, (a & b) << 1); //be careful about the terminating condition;
-        }
     }
 }
